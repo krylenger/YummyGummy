26f04acb103abb21e77891ee4717e904284bd776
krylenger <krylenger@gmail.com>
2021-04-23 21:33:31 +0300

    initial commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { recipesByName, dailyMealPlan } from './apiData';
 
 // console.log(dailyMealPlan);
 
+window.renderApp = renderApp;
+
 window.dataStore = {
   currentGoal: '',
   fridgeItems: [],
@@ -45,8 +47,9 @@ function SetGoal() {
   return `<div>
     <h2>Do you want to lose or to gain weight?</h2>
     ${GoalSwitch(currentGoal, setCurrentGoal)}
-    <p>Meal description ... Meal description ... Meal description ... Meal description ...  </p>
-    <h2>What's your weight?</h2>
+    <p>In case you want to lose weight, we're going to create a meal plan with a decreased amount of carbs.</p>
+    <p>If your goal is to gain muscles - we're going to include more high protein dishes.
+    <h4>What's your weight?</h4>
     <input type="number" value="${usersWeight}" placeholder="your weight" onchange="window.dataStore.usersWeight = this.value; window.renderApp();">
   </div>`;
 }
@@ -81,20 +84,27 @@ window.FillFridgeOnChangeCB = FillFridgeOnChangeCB;
 function FillFridge() {
   const { fridgeItems } = window.dataStore;
   return `<div>
-      <h2>fridge</h2>
-      <p>Enter up to 5 products you have in the fridge to cook the best meal </p>
+      <h2>What's in your fridge?</h2>
+      <p>Enter up to 5 products you have in the fridge to cook the best meal. Example: apple, milk, sugar. </p>
       <input 
         type="text" 
         placeholder="what is in your fridge?" 
-        onchange="window.FillFridgeOnChangeCB(this.value)">
+        onchange="window.FillFridgeOnChangeCB(this.value)"
+      >
+      <button type="submit">+add</button>
       
   </div>`;
 }
 // <input type="submit">
 
+window.confirmButtonCB = confirmButtonCB;
+
+function confirmButtonCB() {}
+
 function RenderFridgeRecipes() {
+  let confirmButton = '';
   if (window.dataStore.fridgeItems.length > 0) {
-    // console.log('hi');
+    confirmButton = `<button onclick="window.confirmButtonCB()" >Magic time!</button>`;
   }
   // console.log(window.dataStore.fridgeItems);
   return `${window.dataStore.fridgeItems
@@ -103,15 +113,17 @@ function RenderFridgeRecipes() {
   ${fridgeItem}
   </div>`,
     )
-    .join('')}`;
+    .join('')}
+    ${confirmButton}`;
 }
 
 function SearchRecipes() {
   return `<div>
+    <h2>Search by recipe name</h2>
     <input 
       type="text" 
       value="${window.dataStore.searchedRecipe}" 
-      placeholder="enter recipe name" 
+      placeholder="enter recipe (ex: rice)" 
       onchange="window.dataStore.searchedRecipe = this.value; window.renderApp()"
     />
 
@@ -131,8 +143,6 @@ function RenderRecipes() {
   `
     : '';
 }
-
-window.renderApp = renderApp;
 
 function renderApp() {
   const appRoot = document.getElementById('app-root');
